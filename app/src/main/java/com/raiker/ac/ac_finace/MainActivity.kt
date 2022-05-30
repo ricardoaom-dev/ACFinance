4c157c4f6b9ba9dac17a93af9de76192200f2d23
Ricardo Ordoñez <ricardoaom.dev@gmail.com>
2022-05-30 15:18:45 -0500

    start project

---
+++ app/src/main/java/com/raiker/ac/ac_finace/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.raiker.ac.ac_finace
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
